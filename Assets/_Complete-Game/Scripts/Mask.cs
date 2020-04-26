ff689f39c2d1b34a034cffd77e88e71f974bbde8
machuofan <b20mcf@hku.hk>
2020-04-26 13:16:14 +0800

    initialization

---
+++ Assets/_Complete-Game/Scripts/Mask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    public float scale = 120f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scale > 25)
+        {
+            scale -= 0.1f;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+}
